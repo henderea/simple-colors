ced79107ce3eef2321b096c6ecce3cd8426d1a69
Eric Henderson <henderea@gmail.com>
2021-08-27 15:41:17 -0400

    add my eslint config for formatting

--- helpText.js
+++ helpText.js
@@ -5,261 +5,261 @@ const { bold, underline, green, cyan } = styles;
 
 const _peek = (array) => array[array.length - 1];
 const _pushAll = (array, ...args) => {
-    _flatten(args).forEach(arg => {
-        array.push(arg);
-    });
-    return array;
+  _flatten(args).forEach((arg) => {
+    array.push(arg);
+  });
+  return array;
 };
 
 class HelpTextMaker {
-    constructor(name) {
-        this._name = name;
-        this._mode = null;
-        this._text = []
-        this._bufferText = [];
-        this._wrap = false;
-        this._wrapIndent = [0];
-        this._counter = 0;
-        this._curIndent = 0;
-        this._inMargin = true;
-    }
+  constructor(name) {
+    this._name = name;
+    this._mode = null;
+    this._text = [];
+    this._bufferText = [];
+    this._wrap = false;
+    this._wrapIndent = [0];
+    this._counter = 0;
+    this._curIndent = 0;
+    this._inMargin = true;
+  }
 
-    pushWrap(wrapIndent = 0) {
-        this._wrapIndent.push(wrapIndent);
-        return this;
-    }
+  pushWrap(wrapIndent = 0) {
+    this._wrapIndent.push(wrapIndent);
+    return this;
+  }
 
-    popWrap() {
-        if(this._wrapIndent.length > 1) {
-            this._wrapIndent.pop();
+  popWrap() {
+    if(this._wrapIndent.length > 1) {
+      this._wrapIndent.pop();
+    }
+    return this;
+  }
+
+  wrap(wrap = true) {
+    this._wrap = wrap;
+    return this;
+  }
+
+  text(...text) {
+    text = _flatten(text);
+    if(this._mode) {
+      let peek = _peek(this._bufferText);
+      if(peek && peek.mode == this._mode) {
+        _pushAll(peek.text, text);
+      } else {
+        this._bufferText.push({ mode: this._mode, text: text, curIndent: this._curIndent });
+      }
+    } else {
+      let wrapIndent = _peek(this._wrapIndent);
+      let peek = _peek(this._text);
+      if(peek && peek.wrapIndent == wrapIndent) {
+        _pushAll(peek.text, text);
+      } else {
+        this._text.push({ wrapIndent, text: text });
+      }
+    }
+    if(text.join('') == '\n') {
+      this._inMargin = true;
+      this._curIndent = 0;
+    } else if(/^ +$/.test(text.join('')) && this._inMargin) {
+      this._curIndent += text.join('').length;
+    } else {
+      this._inMargin = false;
+    }
+    return this;
+  }
+
+  bold(text) {
+    this.text(bold(text));
+    return this;
+  }
+
+  get title() {
+    return this.bold(this._name);
+  }
+
+  get name() {
+    return this.text(this._name);
+  }
+
+  get nl() {
+    return this.text('\n');
+  }
+
+  get tab() {
+    return this.text('    ');
+  }
+
+  get space() {
+    return this.text(' ');
+  }
+
+  get usage() {
+    return this.bold('Usage:');
+  }
+
+  param(param) {
+    return this.text(style(cyan.bright, underline)(param));
+  }
+
+  get flags() {
+    return this.bold('Flags:');
+  }
+
+  get params() {
+    return this.bold('Parameters:');
+  }
+
+  get dict() {
+    this._mode = 'dict';
+    return this;
+  }
+
+  get key() {
+    this._mode = 'key';
+    return this;
+  }
+
+  get value() {
+    this._mode = 'value';
+    return this;
+  }
+
+  get end() {
+    if(this._mode == 'key' || this._mode == 'value') {
+      this._mode = 'dict';
+      return this;
+    }
+    if(this._mode == 'dict') {
+      return this.endDict;
+    }
+    if(this._mode == 'ul' || this._mode == 'ol') {
+      return this.endList;
+    }
+    return this;
+  }
+
+  get endDict() {
+    this._mode = null;
+    let maxKeyLength = _max(this._bufferText.filter((d) => d.mode == 'key').map((d) => style.len(d.text.join(''))));
+    let valueWrapIndent = maxKeyLength + 4;
+    this._bufferText.forEach((d) => {
+      if(d.mode === 'value') {
+        this.pushWrap(valueWrapIndent).tab.text(d.text).popWrap();
+      } else if(d.mode === 'key') {
+        this.text(d.text);
+        let len = style.len(d.text.join(''));
+        if(len < maxKeyLength) {
+          this.text(' '.repeat(maxKeyLength - len));
         }
-        return this;
-    }
+      } else {
+        this.text(d.text);
+      }
+    });
+    this._bufferText = [];
+    return this;
+  }
 
-    wrap(wrap = true) {
-        this._wrap = wrap;
-        return this;
-    }
+  get ul() {
+    this._mode = 'ul';
+    return this;
+  }
 
-    text(...text) {
-        text = _flatten(text);
-        if(this._mode) {
-            let peek = _peek(this._bufferText);
-            if(peek && peek.mode == this._mode) {
-                _pushAll(peek.text, text);
-            } else {
-                this._bufferText.push({ mode: this._mode, text: text, curIndent: this._curIndent });
+  get ol() {
+    this._mode = 'ol';
+    this._counter = 0;
+    return this;
+  }
+
+  get endList() {
+    this._mode = null;
+    let maxLiLength = _max(this._bufferText.filter((d) => d.mode == 'uli' || d.mode == 'oli').map((d) => style.len(d.text.join(''))));
+    this._bufferText.forEach((d) => {
+      if(d.mode === 'uli') {
+        this.pushWrap(maxLiLength + d.curIndent + 1).text(style.padEnd(d.text.join(''), maxLiLength)).space.popWrap();
+      } else if(d.mode === 'oli') {
+        this.pushWrap(maxLiLength + d.curIndent + 2).text(style.padStart(d.text.join(''), maxLiLength)).text(')').space.popWrap();
+      } else {
+        this.pushWrap(maxLiLength + d.curIndent + (d.mode == 'ol' ? 2 : 1)).text(d.text.join('')).popWrap();
+      }
+    });
+    this._bufferText = [];
+    return this;
+  }
+
+  get endUl() {
+    return this.endList;
+  }
+
+  get endOl() {
+    return this.endList;
+  }
+
+  get li() {
+    if(this._mode == 'ul') {
+      this._mode = 'uli';
+      this.bold('\u2022');
+      this._mode = 'ul';
+      return this;
+    }
+    if(this._mode == 'ol') {
+      this._counter++;
+      this._mode = 'oli';
+      this.bold(`${this._counter}`);
+      this._mode = 'ol';
+      return this;
+    }
+    return this;
+  }
+
+  flag(...flags) {
+    return this.text(flags.map((flag) => green.bright(flag)).join(', '));
+  }
+
+  toString(wrap = -1) {
+    if(this._wrap) {
+      if(process.stdout.isTTY && wrap <= 0 || wrap > process.stdout.columns) {
+        wrap = process.stdout.columns;
+      }
+    } else {
+      wrap = -1;
+    }
+    let lastLineLength = 0;
+    return this._text.map((t) => {
+      let lines = t.text.join('').split(/\n/);
+      if(wrap > 0) {
+        lines = lines.map((l) => {
+          let words = l.split(/ /);
+          let wrappedLines = [[]];
+          words.forEach((w) => {
+            let line = _peek(wrappedLines);
+            let lineLength = style.len(line.join(' '));
+            if(wrappedLines.length == 1) {
+              lineLength += lastLineLength;
             }
-        } else {
-            let wrapIndent = _peek(this._wrapIndent);
-            let peek = _peek(this._text);
-            if(peek && peek.wrapIndent == wrapIndent) {
-                _pushAll(peek.text, text);
-            } else {
-                this._text.push({ wrapIndent, text: text });
+            let wordLength = style.len(w);
+            if(lineLength + wordLength + 1 > wrap) {
+              line = [];
+              for(let i = 0; i < t.wrapIndent; i++) {
+                line.push('');
+              }
+              wrappedLines.push(line);
             }
-        }
-        if(text.join('') == '\n') {
-            this._inMargin = true;
-            this._curIndent = 0;
-        } else if(/^ +$/.test(text.join('')) && this._inMargin) {
-            this._curIndent += text.join('').length;
-        } else {
-            this._inMargin = false;
-        }
-        return this;
-    }
-
-    bold(text) {
-        this.text(bold(text));
-        return this;
-    }
-
-    get title() {
-        return this.bold(this._name);
-    }
-
-    get name() {
-        return this.text(this._name);
-    }
-
-    get nl() {
-        return this.text('\n');
-    }
-
-    get tab() {
-        return this.text('    ');
-    }
-
-    get space() {
-        return this.text(' ');
-    }
-
-    get usage() {
-        return this.bold('Usage:');
-    }
-
-    param(param) {
-        return this.text(style(cyan.bright, underline)(param));
-    }
-
-    get flags() {
-        return this.bold('Flags:');
-    }
-
-    get params() {
-        return this.bold('Parameters:');
-    }
-
-    get dict() {
-        this._mode = 'dict';
-        return this;
-    }
-
-    get key() {
-        this._mode = 'key';
-        return this;
-    }
-
-    get value() {
-        this._mode = 'value';
-        return this;
-    }
-
-    get end() {
-        if(this._mode == 'key' || this._mode == 'value') {
-            this._mode = 'dict';
-            return this;
-        }
-        if(this._mode == 'dict') {
-            return this.endDict;
-        }
-        if(this._mode == 'ul' || this._mode == 'ol') {
-            return this.endList;
-        }
-        return this;
-    }
-
-    get endDict() {
-        this._mode = null;
-        let maxKeyLength = _max(this._bufferText.filter(d => d.mode == 'key').map(d => style.len(d.text.join(''))));
-        let valueWrapIndent = maxKeyLength + 4;
-        this._bufferText.forEach(d => {
-            if(d.mode === 'value') {
-                this.pushWrap(valueWrapIndent).tab.text(d.text).popWrap();
-            } else if(d.mode === 'key') {
-                this.text(d.text);
-                let len = style.len(d.text.join(''));
-                if(len < maxKeyLength) {
-                    this.text(' '.repeat(maxKeyLength - len));
-                }
-            } else {
-                this.text(d.text);
-            }
-        });
-        this._bufferText = [];
-        return this;
-    }
-
-    get ul() {
-        this._mode = 'ul';
-        return this;
-    }
-
-    get ol() {
-        this._mode = 'ol';
-        this._counter = 0;
-        return this;
-    }
-
-    get endList() {
-        this._mode = null;
-        let maxLiLength = _max(this._bufferText.filter(d => d.mode == 'uli' || d.mode == 'oli').map(d => style.len(d.text.join(''))));
-        this._bufferText.forEach(d => {
-            if(d.mode === 'uli') {
-                this.pushWrap(maxLiLength + d.curIndent + 1).text(style.padEnd(d.text.join(''), maxLiLength)).space.popWrap();
-            } else if(d.mode === 'oli') {
-                this.pushWrap(maxLiLength + d.curIndent + 2).text(style.padStart(d.text.join(''), maxLiLength)).text(')').space.popWrap();
-            } else {
-                this.pushWrap(maxLiLength + d.curIndent + (d.mode == 'ol' ? 2 : 1)).text(d.text.join('')).popWrap();
-            }
-        });
-        this._bufferText = [];
-        return this;
-    }
-
-    get endUl() {
-        return this.endList;
-    }
-
-    get endOl() {
-        return this.endList;
-    }
-
-    get li() {
-        if(this._mode == 'ul') {
-            this._mode = 'uli';
-            this.bold('\u2022');
-            this._mode = 'ul';
-            return this;
-        }
-        if(this._mode == 'ol') {
-            this._counter++;
-            this._mode = 'oli'
-            this.bold(`${this._counter}`);
-            this._mode = 'ol';
-            return this;
-        }
-        return this;
-    }
-
-    flag(...flags) {
-        return this.text(flags.map(flag => green.bright(flag)).join(', '));
-    }
-
-    toString(wrap = -1) {
-        if(this._wrap) {
-            if(process.stdout.isTTY && wrap <= 0 || wrap > process.stdout.columns) {
-                wrap = process.stdout.columns;
-            }
-        } else {
-            wrap = -1;
-        }
-        let lastLineLength = 0;
-        return this._text.map(t => {
-            let lines = t.text.join('').split(/\n/);
-            if(wrap > 0) {
-                lines = lines.map(l => {
-                    let words = l.split(/ /);
-                    let wrappedLines = [[]];
-                    words.forEach(w => {
-                        let line = _peek(wrappedLines);
-                        let lineLength = style.len(line.join(' '));
-                        if(wrappedLines.length == 1) {
-                            lineLength += lastLineLength;
-                        }
-                        let wordLength = style.len(w);
-                        if(lineLength + wordLength + 1 > wrap) {
-                            line = [];
-                            for(let i = 0; i < t.wrapIndent; i++) {
-                                line.push('');
-                            }
-                            wrappedLines.push(line);
-                        }
-                        line.push(w);
-                    });
-                    lastLineLength = style.len(wrappedLines.map(l => l.join(' ')));
-                    return wrappedLines.map(l => l.join(' ')).join('\n');
-                }).join('\n').split(/\n/);
-            }
-            lastLineLength = style.len(_peek(lines));
-            return lines.join('\n');
-        }).join('');
-    }
+            line.push(w);
+          });
+          lastLineLength = style.len(wrappedLines.map((l) => l.join(' ')));
+          return wrappedLines.map((l) => l.join(' ')).join('\n');
+        }).join('\n').split(/\n/);
+      }
+      lastLineLength = style.len(_peek(lines));
+      return lines.join('\n');
+    }).join('');
+  }
 }
 
 module.exports = {
-    HelpTextMaker,
-    style,
-    styles
+  HelpTextMaker,
+  style,
+  styles
 };
--- index.js
+++ index.js
@@ -10,20 +10,20 @@ const _isStr = require('lodash/isString');
 const supportsColor = require('supports-color');
 const stripAnsi = require('strip-ansi');
 
-const _isNumOrStr = v => _isNum(v) || _isStr(v);
+const _isNumOrStr = (v) => _isNum(v) || _isStr(v);
 
 const useFormat = () => useFormat.force || !!supportsColor.stdout;
 
 useFormat.force = false;
 
 const style = (...sty) => {
-    sty = _uniq(_flatten(sty.map(s => (_isFunc(s) && s._s) ? s._s : s)).filter(s => _isObj(s) && _isNumOrStr(s.o) && _isNumOrStr(s.c)));
-    let o = _uniq(sty.map(s => s.o)).join(';');
-    let c = _uniq(sty.map(s => s.c)).join(';');
-    let func = function(str) { return useFormat() ? `\u001B[${o}m${str}\u001B[${c}m` : str; }
-    func._s = sty;
-    return func;
-}
+  sty = _uniq(_flatten(sty.map((s) => (_isFunc(s) && s._s) ? s._s : s)).filter((s) => _isObj(s) && _isNumOrStr(s.o) && _isNumOrStr(s.c)));
+  let o = _uniq(sty.map((s) => s.o)).join(';');
+  let c = _uniq(sty.map((s) => s.c)).join(';');
+  let func = function(str) { return useFormat() ? `\u001B[${o}m${str}\u001B[${c}m` : str; };
+  func._s = sty;
+  return func;
+};
 
 style.force = (force = true) => { useFormat.force = force; };
 style.forced = () => useFormat.force;
@@ -31,134 +31,134 @@ style.enabled = () => useFormat();
 style.clean = (str) => str && stripAnsi(str);
 style.len = (str) => str ? _stringSize(style.clean(str)) : 0;
 style.pad = (string, length = 0, chars = ' ') => {
-    const strLength = length ? style.len(string) : 0;
-    if(!length || length <= strLength) {
-        return string || '';
-    }
-    const startLength = Math.floor((length - strLength) / 2);
-    const endLength = length - strLength - startLength;
-    return `${_createPadding(startLength, chars)}${string}${_createPadding(endLength, chars)}`;
+  const strLength = length ? style.len(string) : 0;
+  if(!length || length <= strLength) {
+    return string || '';
+  }
+  const startLength = Math.floor((length - strLength) / 2);
+  const endLength = length - strLength - startLength;
+  return `${_createPadding(startLength, chars)}${string}${_createPadding(endLength, chars)}`;
 };
 style.padStart = (string, length = 0, chars = ' ') => {
-    const strLength = length ? style.len(string) : 0;
-    if(!length || length <= strLength) {
-        return string || '';
-    }
-    const startLength = length - strLength;
-    return `${_createPadding(startLength, chars)}${string}`;
+  const strLength = length ? style.len(string) : 0;
+  if(!length || length <= strLength) {
+    return string || '';
+  }
+  const startLength = length - strLength;
+  return `${_createPadding(startLength, chars)}${string}`;
 };
 style.padEnd = (string, length = 0, chars = ' ') => {
-    const strLength = length ? style.len(string) : 0;
-    if(!length || length <= strLength) {
-        return string || '';
-    }
-    const endLength = length - strLength;
-    return `${string}${_createPadding(endLength, chars)}`;
+  const strLength = length ? style.len(string) : 0;
+  if(!length || length <= strLength) {
+    return string || '';
+  }
+  const endLength = length - strLength;
+  return `${string}${_createPadding(endLength, chars)}`;
 };
 
 const brightBg = (normal, bright, bg) => {
-    let sty = style(normal);
-    sty.bright = style(bright);
-    sty.bg = style(bg);
-    return sty;
-}
+  let sty = style(normal);
+  sty.bright = style(bright);
+  sty.bg = style(bg);
+  return sty;
+};
 
 const p = (o, c) => ({ o, c });
 const styles = {
-    bold: style(p(1, 22)),
-    dim: style(p(2, 22)),
-    italic: style(p(3, 23)),
-    underline: style(p(4, 24)),
-    black: brightBg(p(30, 39), p(90, 39), p(40, 49)),
-    red: brightBg(p(31, 39), p(91, 39), p(41, 49)),
-    green: brightBg(p(32, 39), p(92, 39), p(42, 49)),
-    yellow: brightBg(p(33, 39), p(93, 39), p(43, 49)),
-    blue: brightBg(p(34, 39), p(94, 39), p(44, 49)),
-    magenta: brightBg(p(35, 39), p(95, 39), p(45, 49)),
-    cyan: brightBg(p(36, 39), p(96, 39), p(46, 49)),
-    white: brightBg(p(37, 39), p(97, 39), p(47, 49)),
-    customColor(num) {
-        let sty = style(p(`38;5;${num}`, 39));
-        sty.bg = style(p(`48;5;${num}`, 49));
-        return sty;
-    }
+  bold: style(p(1, 22)),
+  dim: style(p(2, 22)),
+  italic: style(p(3, 23)),
+  underline: style(p(4, 24)),
+  black: brightBg(p(30, 39), p(90, 39), p(40, 49)),
+  red: brightBg(p(31, 39), p(91, 39), p(41, 49)),
+  green: brightBg(p(32, 39), p(92, 39), p(42, 49)),
+  yellow: brightBg(p(33, 39), p(93, 39), p(43, 49)),
+  blue: brightBg(p(34, 39), p(94, 39), p(44, 49)),
+  magenta: brightBg(p(35, 39), p(95, 39), p(45, 49)),
+  cyan: brightBg(p(36, 39), p(96, 39), p(46, 49)),
+  white: brightBg(p(37, 39), p(97, 39), p(47, 49)),
+  customColor(num) {
+    let sty = style(p(`38;5;${num}`, 39));
+    sty.bg = style(p(`48;5;${num}`, 49));
+    return sty;
+  }
 };
 
 
 const borderChars = {
-    thin: {
-        topLeft: '\u250c',
-        topRight: '\u2510',
-        bottomLeft: '\u2514',
-        bottomRight: '\u2518',
-        horizontal: '\u2500',
-        vertical: '\u2502'
-    },
-    thick: {
-        topLeft: '\u250f',
-        topRight: '\u2513',
-        bottomLeft: '\u2517',
-        bottomRight: '\u251b',
-        horizontal: '\u2501',
-        vertical: '\u2503'
-    }
+  thin: {
+    topLeft: '\u250c',
+    topRight: '\u2510',
+    bottomLeft: '\u2514',
+    bottomRight: '\u2518',
+    horizontal: '\u2500',
+    vertical: '\u2502'
+  },
+  thick: {
+    topLeft: '\u250f',
+    topRight: '\u2513',
+    bottomLeft: '\u2517',
+    bottomRight: '\u251b',
+    horizontal: '\u2501',
+    vertical: '\u2503'
+  }
 };
 
 const getBorderStylize = (bold, dim) => {
-    if(bold && dim) {
-        return style(styles.bold, styles.dim);
-    } else if(bold) {
-        return styles.bold;
-    } else if(dim) {
-        return styles.dim;
-    } else {
-        return (str) => str;
-    }
-}
+  if(bold && dim) {
+    return style(styles.bold, styles.dim);
+  } else if(bold) {
+    return styles.bold;
+  } else if(dim) {
+    return styles.dim;
+  } else {
+    return (str) => str;
+  }
+};
 
 const applyBorder = (str, bold, dim, thick) => {
-    let len = Math.max(..._flatten([str]).map(style.len));
-    let stylize = getBorderStylize(bold, dim);
-    const chars = thick ? borderChars.thick : borderChars.thin;
-    const topBorder = stylize(`${chars.topLeft}${_repeat(chars.horizontal, len + 2)}${chars.topRight}`);
-    const bottomBorder = stylize(`${chars.bottomLeft}${_repeat(chars.horizontal, len + 2)}${chars.bottomRight}`);
-    const side = stylize(chars.vertical);
-    const middle = _flatten([str]).map(l => `${side} ${style.padEnd(l, len)} ${side}`).join('\n');
-    return `${topBorder}\n${middle}\n${bottomBorder}`;
-}
+  let len = Math.max(..._flatten([str]).map(style.len));
+  let stylize = getBorderStylize(bold, dim);
+  const chars = thick ? borderChars.thick : borderChars.thin;
+  const topBorder = stylize(`${chars.topLeft}${_repeat(chars.horizontal, len + 2)}${chars.topRight}`);
+  const bottomBorder = stylize(`${chars.bottomLeft}${_repeat(chars.horizontal, len + 2)}${chars.bottomRight}`);
+  const side = stylize(chars.vertical);
+  const middle = _flatten([str]).map((l) => `${side} ${style.padEnd(l, len)} ${side}`).join('\n');
+  return `${topBorder}\n${middle}\n${bottomBorder}`;
+};
 
 const borderMaker = (bold = false, dim = false, thick = false) => {
-    const func = (str) => applyBorder(str, bold, dim, thick);
-    const pt = {};
-    Object.defineProperties(pt, {
-        bold: {
-            get: () => borderMaker(true, dim, thick)
-        },
-        dim: {
-            get: () => borderMaker(bold, true, thick)
-        },
-        thick: {
-            get: () => borderMaker(bold, dim, true)
-        },
-        unbold: {
-            get: () => borderMaker(false, dim, thick)
-        },
-        undim: {
-            get: () => borderMaker(bold, false, thick)
-        },
-        thin: {
-            get: () => borderMaker(bold, dim, false)
-        }
-    })
-    func.__proto__ = pt;
-    return func;
-}
+  const func = (str) => applyBorder(str, bold, dim, thick);
+  const pt = {};
+  Object.defineProperties(pt, {
+    bold: {
+      get: () => borderMaker(true, dim, thick)
+    },
+    dim: {
+      get: () => borderMaker(bold, true, thick)
+    },
+    thick: {
+      get: () => borderMaker(bold, dim, true)
+    },
+    unbold: {
+      get: () => borderMaker(false, dim, thick)
+    },
+    undim: {
+      get: () => borderMaker(bold, false, thick)
+    },
+    thin: {
+      get: () => borderMaker(bold, dim, false)
+    }
+  });
+  func.__proto__ = pt;
+  return func;
+};
 
 const border = borderMaker();
 
 module.exports = {
-    style,
-    styles,
-    borderMaker,
-    border
+  style,
+  styles,
+  borderMaker,
+  border
 };
